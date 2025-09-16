6e81ae30c71605b8cca66e7a1f15a1d236969d34
iddob <iddo.ba1768@gmail.com>
2025-09-16 18:54:08 +0300

    first try version of shooting calculation without the regression for the angle calculation
    # Conflicts:
    #	TeamCode/build.gradle

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/calculators/IShooterCalculator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/calculators/IShooterCalculator.java
@@ -1,8 +1,8 @@
 package org.firstinspires.ftc.teamcode.calculators;
 
 import com.pedropathing.geometry.Pose;
-import com.pedropathing.math.Vector;
+import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
 
 public interface IShooterCalculator {
-    public ShootingSolution getShootingSolution(Pose robotPose, Vector goalVelocity);
+    public ShootingSolution getShootingSolution(Pose robotPose, Pose goalPose, Vector3D goalVelocity);
 }
