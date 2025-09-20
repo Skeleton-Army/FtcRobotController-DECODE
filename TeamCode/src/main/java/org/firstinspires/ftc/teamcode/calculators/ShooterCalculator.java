08e179945f085ca49e9825ee1c284860d4fe6efb
iddob <iddo.ba1768@gmail.com>
2025-09-20 19:38:49 +0300

    Added velocity compensation for different ranges + some javadoc

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/calculators/ShooterCalculator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/calculators/ShooterCalculator.java
@@ -17,11 +17,26 @@ public class ShooterCalculator implements IShooterCalculator {
     private static final String coefficientsFilePath = "Coefficients.json";
     private final ObjectMapper mapper = new ObjectMapper();
 
-    private final double shooterVelocity = 2;
+    private final double shooterMinVelocity = -1;
+    private final double shooterMaxVelocity = -1;
+
+    private double shooterVelocity(double distance) {
+        return distance * (shooterMaxVelocity - shooterMinVelocity);
+    }
 
     public ShooterCalculator() throws IOException {
         ObjectMapper mapper = new ObjectMapper();
         this.model = mapper.readValue(new File(coefficientsFilePath), Model.class);
+    }
+
+    /**
+     *
+     * @param metersPerSecond Starting velocity in meters/second
+     * @return Motor RPM
+     */
+    public double convertVelocityToMotorRPM(double metersPerSecond) {
+        //TODO: Find convertion with regression (probably linear but could be parabolic)
+        return -1;
     }
 
     private double approximateStaticVerticalAngle(double distanceFromGoal) {
@@ -32,12 +47,19 @@ public class ShooterCalculator implements IShooterCalculator {
         return result;
     }
 
+    /**
+     *
+     * @param robotPose Robot position (pedro field coordinates)
+     * @param goalPose Goal position (pedro field coordinates)
+     * @param velocity Robot velocity vector in meters per second
+     * @return ShootingSolution at field coordinates (e.g. horizontal angle at relative to the field and not the robot)
+     */
     public ShootingSolution getShootingSolution(Pose robotPose, Pose goalPose, Vector3D velocity) {
         double distanceFromGoal = robotPose.distanceFrom(goalPose);
         double staticVerticalAngle = approximateStaticVerticalAngle(distanceFromGoal);
         double horizontalAngle = Math.atan2(goalPose.getX() - robotPose.getX(), goalPose.getY() - robotPose.getY());
         Vector3D targetVelocityBase = new Vector3D(staticVerticalAngle, horizontalAngle);
-        Vector3D targetVelocity = new Vector3D(shooterVelocity, targetVelocityBase);
+        Vector3D targetVelocity = new Vector3D(shooterVelocity(distanceFromGoal), targetVelocityBase);
         Vector3D v0 = targetVelocity.add(velocity.negate());
 
         return new ShootingSolution(v0.getDelta(), v0.getAlpha(), v0.getNorm()); //might be mistake from wrong placement of alpha and delta
