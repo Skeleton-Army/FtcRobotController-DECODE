8898439e5f13a5fe1ac705c8c2be7790dcf84477
AmiT1458 <97545597+AmiT1458@users.noreply.github.com>
2025-09-23 19:03:47 +0300

    feat: added driver control and converted from meters to inches

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/LimelightToAdvantageScope.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/LimelightToAdvantageScope.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.opModes;
 
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.pedropathing.follower.Follower;
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.LLStatus;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
@@ -10,31 +11,43 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 
 import java.util.logging.Logger;
 
-@TeleOp(name = "limelight apriltag")
+@TeleOp(name = "limelight apriltag", group = "limelight")
 public class LimelightToAdvantageScope extends OpMode {
 
 
     TelemetryPacket telemetryPacket;
     FtcDashboard dashboard;
     private Limelight3A limelight;
-    Telemetry dashboardTelemetry;
+    private Follower follower;
     @Override
     public void init() {
         telemetryPacket = new TelemetryPacket();
         dashboard = FtcDashboard.getInstance();
-        dashboardTelemetry = dashboard.getTelemetry();
 
         limelight = hardwareMap.get(Limelight3A.class, "limelight");
         telemetry.setMsTransmissionInterval(11);
         limelight.pipelineSwitch(0);
         limelight.start();
+
+        follower = Constants.createFollower(hardwareMap);
+        follower.startTeleopDrive(true);
+
     }
 
     @Override
     public void loop() {
+        // enabling driver control while detecting apriltag
+        follower.update();
+        follower.setTeleOpDrive(-gamepad1.left_stick_y, -gamepad1.left_stick_x, -gamepad1.right_stick_x, true);
+
+        // robot's movement stats
+        telemetryPacket.put("velocity", follower.getVelocity());
+        telemetryPacket.put("acceleration", follower.getAcceleration());
+
         LLStatus status = limelight.getStatus();
         LLResult result = limelight.getLatestResult();
         if (result.isValid()) {
@@ -46,22 +59,17 @@ public class LimelightToAdvantageScope extends OpMode {
             telemetryPacket.put("Pose z", botpose.getPosition().z);
             telemetryPacket.put("Pose heading", botpose.getOrientation().getYaw());
 
-            dashboardTelemetry.addData("Pose x", botpose.getPosition().x);
-            dashboardTelemetry.addData("Pose y", botpose.getPosition().y);
-            dashboardTelemetry.addData("Pose z", botpose.getPosition().z);
-            dashboardTelemetry.addData("Pose heading", botpose.getOrientation().getYaw());
-
             telemetry.addData("Pose x", botpose.getPosition().x);
             telemetry.addData("Pose y", botpose.getPosition().y);
             telemetry.addData("Pose Z", botpose.getPosition().z);
         }
 
+        // limelights stats
         telemetryPacket.put("Cpu", status.getCpu());
         telemetryPacket.put("Ram", status.getRam());
-        telemetryPacket.put("result: ", result);
+        telemetryPacket.put("Ram", status.getFps());
 
         telemetry.update();
-        dashboardTelemetry.update();
         dashboard.sendTelemetryPacket(telemetryPacket);
 
     }
