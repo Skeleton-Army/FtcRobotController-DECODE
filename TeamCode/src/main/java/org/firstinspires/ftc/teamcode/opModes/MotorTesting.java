e6bd86f84c86c7719b81b78f822e4bc38d88c533
opi3636 <yoyozohar2010@gmail.com>
2025-09-27 19:42:04 +0300

    wip: Add the prompts for the Motor Testing opMode
    # Conflicts:
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/MotorTesting.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/MotorTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/MotorTesting.java
@@ -19,35 +19,25 @@ public class MotorTesting extends OpMode {
     String[] motorNames;
     Prompter prompter = new Prompter(this);
     enum MotorType {
-        DC_MOTOR,
-        SERVO
-    }
-    enum TestType {
-        SIMULTANEOUS_TESTING,
-        INDIVIDUAL_TESTING
+        DcMotor,
+        Servo
     }
 
     @Override
     public void init() {
-        prompter.prompt("motorType", new OptionPrompt<>("Select motor type to test", MotorType.DC_MOTOR, MotorType.SERVO))
+        prompter.prompt("motorType", new OptionPrompt<>("Select motor type to test", MotorType.DcMotor, MotorType.Servo))
                 // TODO: once Multiple Option prompt is out replace this to be able to select multiple motors
                 .prompt("motorToTest", () -> {
                     final MotorType selectedMotor = prompter.get("motorType");
-                   if ( selectedMotor == MotorType.DC_MOTOR) {
+                   if ( selectedMotor == MotorType.DcMotor) {
                         dcMotorMap = initAllDevices(DcMotorEx.class);
                         motorNames = dcMotorMap.keySet().toArray(new String[0]);
                         return new OptionPrompt<>("Select motor to run", motorNames);
-                   } else if (selectedMotor == MotorType.SERVO) {
+                   } else if (selectedMotor == MotorType.Servo) {
                        servoMap = initAllDevices(Servo.class);
                        motorNames = servoMap.keySet().toArray(new String[0]);
                        return new OptionPrompt<>("Select servo to run", motorNames);
                    }
-                    return null;
-                })
-                .prompt("testType", () -> {
-                    if (prompter.get("motorType") == MotorType.DC_MOTOR) {
-                        return new OptionPrompt<>("Select test type", TestType.INDIVIDUAL_TESTING, TestType.SIMULTANEOUS_TESTING);
-                    }
                     return null;
                 })
 
@@ -58,17 +48,17 @@ public class MotorTesting extends OpMode {
                  so the prompt below will no be needed once it is implemented and make not hard coded and just less spaghetti
                 */
                 .prompt("dcMotorRPM", () -> {
-                    if (prompter.get("motorType") == MotorType.DC_MOTOR) {
+                    if (prompter.get("motorType") == MotorType.DcMotor) {
                         return new OptionPrompt<>("Select the max RPM of motor: " + prompter.get("motorToTest"),
                                 "6,000", "1,620", "1,150", "435", "312", "223", "117", "100", "84", "60", "43", "30");
                     }
                     return null;
                 })
                 .prompt("startTest", () -> {
-                    if (prompter.get("motorType") == MotorType.DC_MOTOR) {
+                    if (prompter.get("motorType") == MotorType.DcMotor) {
                         return new BooleanPrompt(String.format("Start test for motor %s at %s RPM?",
                                 prompter.get("motorToTest"), prompter.get("dcMotorRPM")), true);
-                    } else if (prompter.get("motorType") == MotorType.SERVO) {
+                    } else if (prompter.get("motorType") == MotorType.Servo) {
                         return new BooleanPrompt(String.format("Start test for motor %s?", prompter.get("motorToTest")), true);
                     }
                     return null;
@@ -95,26 +85,22 @@ public class MotorTesting extends OpMode {
     }
 
     private void startTest() {
+        boolean startTest = prompter.get("startTest");
         MotorType motorType;
         int motorMaxRPM;
         String motorName;
 
-        if (prompter.get("startTest").equals(false)) {
+        if (!startTest) {
             telemetry.addLine("Stopping opMode");
             requestOpModeStop();
         }
 
         motorType = prompter.get("motorType");
         motorName = prompter.get("motorToTest");
-        if (motorType == MotorType.SERVO) {
-            // run Servo test
+        if (motorType == MotorType.Servo) {
+            // TODO: run Servo test
         }
         motorMaxRPM = Integer.parseInt(prompter.get("dcMotorRPM"));
-
-        if (prompter.get("testType") == TestType.SIMULTANEOUS_TESTING) {
-            // run Simultaneous motor testing
-        } else {
-            // run Individual motor testing
-        }
+        // TODO: run DcMotor test
     }
 }
