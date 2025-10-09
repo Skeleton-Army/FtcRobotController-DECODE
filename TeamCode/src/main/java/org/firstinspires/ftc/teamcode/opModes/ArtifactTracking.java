6863e421bb0576f0057f4703468b9c93dc0ef140
Skeleton Army <skeleton.army23644@gmail.com>
2025-10-09 19:30:18 +0300

    wip: artifact tracking v0.2 DOESN'T WORK (yet)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/ArtifactTracking.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/ArtifactTracking.java
@@ -1,33 +1,40 @@
 package org.firstinspires.ftc.teamcode.opModes;
 
+import android.media.AudioMixerAttributes;
+
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.opencv.core.Mat;
+
 @TeleOp
 public class ArtifactTracking extends OpMode {
+
+    double LLhight = 9.7;
 
     private Limelight3A limelight3A;
     @Override
     public void init() {
         limelight3A = hardwareMap.get(Limelight3A.class, "limelight");
-        limelight3A.pipelineSwitch(0); //pipeline 0 is for purple ball and 1 is for green.
-    }
-    @Override
-    public void start(){
+        limelight3A.pipelineSwitch(0);
         limelight3A.start();
     }
+
     @Override
     public void loop() {
         LLResult llResult = limelight3A.getLatestResult();
-        if (llResult != null && llResult.isValid()){
-            telemetry.addData("Target X offset", llResult.getTx());
-            telemetry.addData("Target y offset", llResult.getTy());
-            telemetry.addData("Target Area offset", llResult.getTa());
-            telemetry.addLine("S1mple");
-        }
 
+        if (llResult != null && llResult.isValid()) {
+            double ty = llResult.getTy();
+            double dx = LLhight * Math.tan(Math.toRadians(ty+90));
+            double tx = Math.toRadians(llResult.getTx());
+            double dy = Math.tan(tx)/dx;
+            telemetry.addData("Artifact X", dx);
+            telemetry.addData("Artifact Y", dy);
+            telemetry.update();
+        }
     }
 }
 
