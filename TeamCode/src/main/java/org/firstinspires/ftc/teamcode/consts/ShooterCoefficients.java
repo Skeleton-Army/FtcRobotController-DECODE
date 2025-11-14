3066f282ff524eed0b8c9cbffca766845c2ae7bf
iddob <iddo.ba1768@gmail.com>
2025-11-14 11:48:42 +0200

    fix: calibrated values with measured velocity

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/consts/ShooterCoefficients.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/consts/ShooterCoefficients.java
@@ -2,6 +2,6 @@ package org.firstinspires.ftc.teamcode.consts;
 
 public class ShooterCoefficients {
     public static double[] velCoeffs = {0, 0, 0, 0};
-    public static double[] hoodCoeffs = {0.0186, -0.1732, 0.6921, -1.3536, 1.6110};
+    public static double[] hoodCoeffs = {0.0313, 0.2975, 1.0686, 1.7078, 1.4369};
 }
 
