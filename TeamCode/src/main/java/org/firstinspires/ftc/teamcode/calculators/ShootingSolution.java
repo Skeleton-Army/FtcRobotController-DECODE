48749899d9832a105f30b1ac3004e4e815dd48ce
Nowam <43613100+Nowammm@users.noreply.github.com>
2025-12-28 17:05:46 +0200

    Revert "feat/add ball trajectory and recovery calculation"
    This reverts commit 77af21ea

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/calculators/ShootingSolution.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/calculators/ShootingSolution.java
@@ -4,13 +4,11 @@ public class ShootingSolution {
     private final double horizontalAngle;
     private final double verticalAngle;
     private final double flywheelRPM;
-    private final double exitVelocity;
 
-    public ShootingSolution(double horizontalAngle, double verticalAngle, double flywheelRPM, double exitVelocity) {
+    public ShootingSolution(double horizontalAngle, double verticalAngle, double flywheelRPM) {
         this.horizontalAngle = horizontalAngle;
         this.verticalAngle = verticalAngle;
         this.flywheelRPM = flywheelRPM;
-        this.exitVelocity = exitVelocity;
     }
 
     public double getHorizontalAngle() {
@@ -21,8 +19,5 @@ public class ShootingSolution {
     }
     public double getVelocity() {
         return flywheelRPM;
-    }
-    public double getVelocityMetersPerSec() {
-        return exitVelocity;
     }
 }
