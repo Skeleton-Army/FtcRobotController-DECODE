7d43e5f875ac87b10594bb314a7e08c4108b4f89
iddob <iddo.ba1768@gmail.com>
2026-02-17 22:44:46 +0200

    change to public

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/consts/ShooterCoefficients.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/consts/ShooterCoefficients.java
@@ -9,9 +9,9 @@ public class ShooterCoefficients {
     public static double[] CLOSE_VEL_COEFFS = {415, 100};
     public static double[] FAR_VEL_COEFFS = {415, 100};
     public static final double DISTANCE_THRESHOLD_METERS = 2.5;
-    private static final double[] HOOD_COEFFS = {-0.01019346, 0.15389212, -0.91188978, 2.65911739, -3.89362224, 3.03329116};
+    public static final double[] HOOD_COEFFS = {-0.01019346, 0.15389212, -0.91188978, 2.65911739, -3.89362224, 3.03329116};
 
-    private static final double[] MIN_VEL_COEFFS = {0.02770411, -0.38846154, 2.07521833, -5.17235799, 6.88146406, 1.32267170};
+    public static final double[] MIN_VEL_COEFFS = {0.02770411, -0.38846154, 2.07521833, -5.17235799, 6.88146406, 1.32267170};
 
-    private static final double[] MAX_VEL_COEFFS = {-0.01138695, 0.13700431, -0.58997567, 1.04296726, 0.59121101, 3.55756520};
+    public static final double[] MAX_VEL_COEFFS = {-0.01138695, 0.13700431, -0.58997567, 1.04296726, 0.59121101, 3.55756520};
 }
