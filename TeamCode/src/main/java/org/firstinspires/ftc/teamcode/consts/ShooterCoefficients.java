63314e36a1faddd3fc2e3635b711d6dde15d3a7c
iddob <iddo.ba1768@gmail.com>
2026-02-17 22:43:43 +0200

    please work this time

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/consts/ShooterCoefficients.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/consts/ShooterCoefficients.java
@@ -5,13 +5,13 @@ import com.acmerobotics.dashboard.config.Config;
 @Config
 public class ShooterCoefficients {
     public static double[] RPM_INTERPOLATION = {420, 0};
-    public static double[] VELOCITY_INTERPOLATION = {1.1673, 3.5767};
+    public static double[] VELOCITY_INTERPOLATION = {1.1413, 3.6006};
     public static double[] CLOSE_VEL_COEFFS = {415, 100};
     public static double[] FAR_VEL_COEFFS = {415, 100};
     public static final double DISTANCE_THRESHOLD_METERS = 2.5;
-    public static final double[] HOOD_COEFFS = {-0.01019346, 0.15389212, -0.91188978, 2.65911739, -3.89362224, 3.03329116};
+    private static final double[] HOOD_COEFFS = {-0.01019346, 0.15389212, -0.91188978, 2.65911739, -3.89362224, 3.03329116};
 
-    public static final double[] MIN_VEL_COEFFS = {0.02563666, -0.36066151, 1.93293321, -4.83018900, 6.50218727, 1.47262155};
+    private static final double[] MIN_VEL_COEFFS = {0.02770411, -0.38846154, 2.07521833, -5.17235799, 6.88146406, 1.32267170};
 
-    public static final double[] MAX_VEL_COEFFS = {-0.00244035, -0.00276436, 0.26471416, -1.49694056, 4.20831507, 1.75071217};
+    private static final double[] MAX_VEL_COEFFS = {-0.01138695, 0.13700431, -0.58997567, 1.04296726, 0.59121101, 3.55756520};
 }
