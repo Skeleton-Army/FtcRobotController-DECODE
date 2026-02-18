9fa140130720b8ad714e9bfe1d92177b96759ff3
angrybirdsFTC <angrybirdsftc@gmail.com>
2026-02-18 18:58:41 +0200

    fix: increase target height to reduce bounceouts

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/consts/ShooterCoefficients.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/consts/ShooterCoefficients.java
@@ -9,9 +9,7 @@ public class ShooterCoefficients {
     public static double[] CLOSE_VEL_COEFFS = {415, 100};
     public static double[] FAR_VEL_COEFFS = {415, 100};
     public static final double DISTANCE_THRESHOLD_METERS = 2.5;
-    public static final double[] HOOD_COEFFS = {-0.01019346, 0.15389212, -0.91188978, 2.65911739, -3.89362224, 3.03329116};
-
-    public static final double[] MIN_VEL_COEFFS = {0.02770411, -0.38846154, 2.07521833, -5.17235799, 6.88146406, 1.32267170};
-
-    public static final double[] MAX_VEL_COEFFS = {-0.01138695, 0.13700431, -0.58997567, 1.04296726, 0.59121101, 3.55756520};
+    public static final double[] HOOD_COEFFS = {-0.00842119, 0.12090451, -0.67832152, 1.87398840, -2.66079694, 2.38229058};
+    public static final double[] MIN_VEL_COEFFS = {-0.00753753, 0.14158881, -1.01421641, 3.47785831, -4.57624623, 6.68692950};
+    public static final double[] MAX_VEL_COEFFS = {0.01871128, -0.26276313, 1.42527417, -3.70961862, 5.72298206, 1.59238723};
 }
