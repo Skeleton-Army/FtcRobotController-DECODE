fd5436c7a6e89fb1f9595b8ff418aafd874be2f7
Skeleton Army <skeleton.army23644@gmail.com>
2026-02-20 17:48:17 +0200

    fix: new kickstand min value

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/KickstandConfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/KickstandConfig.java
@@ -5,6 +5,6 @@ import com.acmerobotics.dashboard.config.Config;
 @Config
 public class KickstandConfig {
     public static String SERVO_NAME = "kickstand";
-    public static double SERVO_MIN = 0;
+    public static double SERVO_MIN = 0.05;
     public static double SERVO_MAX = 1;
 }
