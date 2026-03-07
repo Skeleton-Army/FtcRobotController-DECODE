2b7c15cfeac030e54ed23f26b276d5c05b6edc9f
angrybirdsFTC <angrybirdsftc@gmail.com>
2026-03-07 16:15:08 +0200

    fix: flip flywheel motor direction

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/ShooterConfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/ShooterConfig.java
@@ -9,8 +9,8 @@ public class ShooterConfig {
     public static String FLYWHEEL1_NAME = "flywheel1";
     public static String FLYWHEEL2_NAME = "flywheel2";
     public static GoBILDA FLYWHEEL_MOTOR = GoBILDA.BARE;
-    public static boolean FLYWHEEL1_INVERTED = false;
-    public static boolean FLYWHEEL2_INVERTED = true;
+    public static boolean FLYWHEEL1_INVERTED = true;
+    public static boolean FLYWHEEL2_INVERTED = false;
     public static int RPM_REACHED_THRESHOLD = 200;
     public static double FLYWHEEL_KP = 5;
     public static double FLYWHEEL_KP_DOWN = 16;
