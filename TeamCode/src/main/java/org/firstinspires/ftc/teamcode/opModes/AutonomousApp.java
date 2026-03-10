b452e420bdccc6e8b54f39689b6f0203a3a90522
angrybirdsFTC <angrybirdsftc@gmail.com>
2026-03-10 16:04:29 +0200

    refactor: remove unused pose setting

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/AutonomousApp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/AutonomousApp.java
@@ -595,11 +595,6 @@ public class AutonomousApp extends ComplexOpMode {
         prompter.run();
     }
 
-    @Override
-    public void end() {
-        Settings.set("pose", follower.getPose(), false);
-    }
-
     private Pose getRelative(Pose originalPose) {
         if (alliance == Alliance.RED) {
             return originalPose.mirror();
