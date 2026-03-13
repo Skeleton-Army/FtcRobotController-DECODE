0f24a54c31d38177867a6113719a05f1311638e0
opi3636 <yoyozohar2010@gmail.com>
2026-03-13 16:45:40 +0200

    feat: early return if too far from april tag

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Vision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Vision.java
@@ -85,6 +85,7 @@ public class Vision extends SubsystemBase {
 
     public boolean relocalize() {
         if (pipeline == OBELISK_PIPELINE) return false;
+        if (poseTracker.getPose().getY() < 72) return false;
 
         double velocity = poseTracker.getVelocity().getMagnitude();
         double angularVelocity = poseTracker.getAngularVelocity();
