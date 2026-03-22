aa6b11f84325e91bca5d52111c3ca947829ccb8c
angrybirdsFTC <angrybirdsftc@gmail.com>
2026-03-22 20:20:25 +0200

    fix: change far goal pose

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/consts/GoalPositions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/consts/GoalPositions.java
@@ -8,6 +8,6 @@ public class GoalPositions {
     public final static Pose BLUE_GOAL = new Pose(2, FIELD_LENGTH - 2);
     public final static Pose RED_GOAL = new Pose(FIELD_LENGTH - BLUE_GOAL.getX(), BLUE_GOAL.getY());
 
-    public final static Pose BLUE_GOAL_FAR = new Pose(10, FIELD_LENGTH - 2);
+    public final static Pose BLUE_GOAL_FAR = new Pose(7, FIELD_LENGTH - 2);
     public final static Pose RED_GOAL_FAR = new Pose(FIELD_LENGTH - BLUE_GOAL_FAR.getX(), BLUE_GOAL_FAR.getY());
 }
