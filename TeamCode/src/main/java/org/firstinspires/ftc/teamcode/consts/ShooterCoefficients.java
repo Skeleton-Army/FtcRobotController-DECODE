40d031693ba9b0dfb6a5f2f7ebb8f657df0aa911
angrybirdsFTC <angrybirdsftc@gmail.com>
2026-06-30 19:31:23 +0300

    Revert "fix: reduce RPM_INTERPOLATION to 470"
    This reverts commit f6a888841d811214f46f8e2b0d822d9a9c54e878.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/consts/ShooterCoefficients.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/consts/ShooterCoefficients.java
@@ -4,7 +4,7 @@ import com.acmerobotics.dashboard.config.Config;
 
 @Config
 public class ShooterCoefficients {
-    public double[] RPM_INTERPOLATION = {470, 0};
+    public double[] RPM_INTERPOLATION = {475, 0};
     public double[] VELOCITY_INTERPOLATION = {0.9722, 3.7194};
     public static final double[] MIN_VEL_COEFFS = {-0.01508226, 0.21624210, -1.20411612, 3.23962373, -3.19567987, 5.49066643};
     public static final double[] MAX_VEL_COEFFS = {0.01038346, -0.13950784, 0.71383673, -1.71093057, 2.82016923, 3.14727338};
