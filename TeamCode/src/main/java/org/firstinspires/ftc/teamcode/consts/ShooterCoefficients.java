0b653c96fbfc6a3f98e478971f5e9ca7161fa71b
iddob <iddo.ba1768@gmail.com>
2026-07-11 13:53:58 +0300

    feat: adjust shooting parameters for cri field

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/consts/ShooterCoefficients.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/consts/ShooterCoefficients.java
@@ -6,7 +6,9 @@ import com.acmerobotics.dashboard.config.Config;
 public class ShooterCoefficients {
     public double[] RPM_INTERPOLATION = {475, 0};
     public double[] VELOCITY_INTERPOLATION = {0.9722, 3.7194};
-    public static final double[] MIN_VEL_COEFFS = {-0.01508226, 0.21624210, -1.20411612, 3.23962373, -3.19567987, 5.49066643};
-    public static final double[] MAX_VEL_COEFFS = {0.01038346, -0.13950784, 0.71383673, -1.71093057, 2.82016923, 3.14727338};
-    public static final double[] HOOD_COEFFS = {-0.00526129, 0.07453143, -0.41918836, 1.17898518, -1.68283754, 1.83040709};
+    public static final double[] MIN_VEL_COEFFS = {-0.00528470, 0.09206074, -0.61495762, 1.95117865, -1.91193896, 5.04735174};
+
+    public static final double[] MAX_VEL_COEFFS = {0.00222497, -0.03698359, 0.23121640, -0.66246109, 1.79566580, 3.50321521};
+
+    public static final double[] HOOD_COEFFS = {-0.00152664, 0.02829630, -0.20423260, 0.71309074, -1.22392859, 1.66816074};
 }
