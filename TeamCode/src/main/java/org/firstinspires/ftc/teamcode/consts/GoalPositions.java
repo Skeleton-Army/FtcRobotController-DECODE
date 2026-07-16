4f52d1e9a07ee1f7e81001039b7a32ae4003331a
Skeleton Army <skeleton.army23644@gmail.com>
2026-07-16 11:58:11 +0300

    fix: use field length of 192 for goal positions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/consts/GoalPositions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/consts/GoalPositions.java
@@ -4,18 +4,19 @@ import com.pedropathing.geometry.Pose;
 
 public class GoalPositions {
     public final static double FIELD_LENGTH = 189.5;
-    public final static double HALF_FIELD_LENGTH = FIELD_LENGTH/2;
+    public final static double FULL_FIELD_LENGTH = 192;
+    public final static double HALF_FIELD_LENGTH = FULL_FIELD_LENGTH / 2;
 
-    public final static Pose BLUE_GOAL = new Pose(10, FIELD_LENGTH - 5);
-    public final static Pose RED_GOAL = new Pose(FIELD_LENGTH - BLUE_GOAL.getX(), BLUE_GOAL.getY());
+    public final static Pose BLUE_GOAL = new Pose(10, FULL_FIELD_LENGTH - 5);
+    public final static Pose RED_GOAL = new Pose(FULL_FIELD_LENGTH - BLUE_GOAL.getX(), BLUE_GOAL.getY());
 
-    public final static Pose RED_GOAL_GEMS = new Pose (HALF_FIELD_LENGTH + BLUE_GOAL.getX(),BLUE_GOAL.getY());
-    public final static Pose BLUE_GOAL_GEMS = new Pose(HALF_FIELD_LENGTH - BLUE_GOAL.getX(),BLUE_GOAL.getY());
+    public final static Pose RED_GOAL_GEMS = new Pose (HALF_FIELD_LENGTH + BLUE_GOAL.getX(), BLUE_GOAL.getY());
+    public final static Pose BLUE_GOAL_GEMS = new Pose(HALF_FIELD_LENGTH - BLUE_GOAL.getX(), BLUE_GOAL.getY());
 
-    public final static Pose BLUE_GOAL_FAR = new Pose(6, FIELD_LENGTH);
-    public final static Pose RED_GOAL_FAR = new Pose(FIELD_LENGTH - BLUE_GOAL_FAR.getX(), BLUE_GOAL_FAR.getY());
+    public final static Pose BLUE_GOAL_FAR = new Pose(6, FULL_FIELD_LENGTH);
+    public final static Pose RED_GOAL_FAR = new Pose(FULL_FIELD_LENGTH - BLUE_GOAL_FAR.getX(), BLUE_GOAL_FAR.getY());
 
-    public final static Pose CLOSE_AUTO_BLUE_GOAL = new Pose(6, FIELD_LENGTH - 2);
-    public final static Pose CLOSE_AUTO_RED_GOAL = new Pose(FIELD_LENGTH - CLOSE_AUTO_BLUE_GOAL.getX(), CLOSE_AUTO_BLUE_GOAL.getY());
+    public final static Pose CLOSE_AUTO_BLUE_GOAL = new Pose(6, FULL_FIELD_LENGTH - 2);
+    public final static Pose CLOSE_AUTO_RED_GOAL = new Pose(FULL_FIELD_LENGTH - CLOSE_AUTO_BLUE_GOAL.getX(), CLOSE_AUTO_BLUE_GOAL.getY());
 
 }
