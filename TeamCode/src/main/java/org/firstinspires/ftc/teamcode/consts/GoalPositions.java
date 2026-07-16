2b0c718ad970318c8341b99305007b6d4f1b1b01
Skeleton Army <skeleton.army23644@gmail.com>
2026-07-16 19:07:07 +0300

    fix: FUCK THESE GOAL POSITIONS I HATE CRI

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/consts/GoalPositions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/consts/GoalPositions.java
@@ -1,22 +1,21 @@
 package org.firstinspires.ftc.teamcode.consts;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.pedropathing.geometry.Pose;
 
+@Config
 public class GoalPositions {
     public final static double FIELD_LENGTH = 189.5;
-    public final static double FULL_FIELD_LENGTH = 192;
-    public final static double HALF_FIELD_LENGTH = FULL_FIELD_LENGTH / 2;
+    public final static double HALF_FIELD_LENGTH = 189.5 / 2;
 
-    public final static Pose BLUE_GOAL = new Pose(10, FULL_FIELD_LENGTH - 5);
-    public final static Pose RED_GOAL = new Pose(FULL_FIELD_LENGTH - BLUE_GOAL.getX(), BLUE_GOAL.getY());
+    public final static Pose BLUE_GOAL = new Pose(10, FIELD_LENGTH - 5);
+    public final static Pose RED_GOAL = new Pose(FIELD_LENGTH - BLUE_GOAL.getX(), BLUE_GOAL.getY());
 
     public final static Pose RED_GOAL_GEMS = new Pose (HALF_FIELD_LENGTH + BLUE_GOAL.getX(), BLUE_GOAL.getY());
     public final static Pose BLUE_GOAL_GEMS = new Pose(HALF_FIELD_LENGTH - BLUE_GOAL.getX(), BLUE_GOAL.getY());
 
-    public final static Pose BLUE_GOAL_FAR = new Pose(6, FULL_FIELD_LENGTH);
-    public final static Pose RED_GOAL_FAR = new Pose(FULL_FIELD_LENGTH - BLUE_GOAL_FAR.getX(), BLUE_GOAL_FAR.getY());
-
-    public final static Pose CLOSE_AUTO_BLUE_GOAL = new Pose(6, FULL_FIELD_LENGTH - 2);
-    public final static Pose CLOSE_AUTO_RED_GOAL = new Pose(FULL_FIELD_LENGTH - CLOSE_AUTO_BLUE_GOAL.getX(), CLOSE_AUTO_BLUE_GOAL.getY());
-
+    private final static double GOOFY_OFFSET_X = 10; // For some mysterious splendid wonderful reason this is needed because it shoots way left and i have NO clue why
+    private final static double FAR_OFFSET_X = 5;
+    public final static Pose BLUE_GOAL_FAR = new Pose(GOOFY_OFFSET_X + FAR_OFFSET_X, FIELD_LENGTH);
+    public final static Pose RED_GOAL_FAR = new Pose(FIELD_LENGTH + FAR_OFFSET_X, BLUE_GOAL_FAR.getY());
 }
