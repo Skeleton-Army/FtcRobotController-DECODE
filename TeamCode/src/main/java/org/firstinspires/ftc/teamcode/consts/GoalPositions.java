f0a6a28ce687c8e3e713599ffe502f5aea942a6b
Skeleton Army <skeleton.army23644@gmail.com>
2026-07-17 17:26:09 +0300

    fix: idfk anymore

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/consts/GoalPositions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/consts/GoalPositions.java
@@ -13,10 +13,6 @@ public class GoalPositions {
     public final static Pose RED_GOAL_GEMS = new Pose (HALF_FIELD_LENGTH + BLUE_GOAL.getX(), BLUE_GOAL.getY());
     public final static Pose BLUE_GOAL_GEMS = new Pose(HALF_FIELD_LENGTH - BLUE_GOAL.getX(), BLUE_GOAL.getY());
 
-    public final static Pose BLUE_GOAL_FAR = new Pose(6, FULL_FIELD_LENGTH);
+    public final static Pose BLUE_GOAL_FAR = new Pose(10, FULL_FIELD_LENGTH);
     public final static Pose RED_GOAL_FAR = new Pose(FULL_FIELD_LENGTH - BLUE_GOAL_FAR.getX(), BLUE_GOAL_FAR.getY());
-
-    public final static Pose CLOSE_AUTO_BLUE_GOAL = new Pose(6, FULL_FIELD_LENGTH - 2);
-    public final static Pose CLOSE_AUTO_RED_GOAL = new Pose(FULL_FIELD_LENGTH - CLOSE_AUTO_BLUE_GOAL.getX(), CLOSE_AUTO_BLUE_GOAL.getY());
-
 }
