98e524e7a902a0e6c5a6a2f518d16dd872623826
Skeleton Army <skeleton.army23644@gmail.com>
2026-07-18 12:23:42 +0300

    fix: tune shooting parameters

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/consts/GoalPositions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/consts/GoalPositions.java
@@ -7,12 +7,12 @@ public class GoalPositions {
     public final static double FULL_FIELD_LENGTH = 192;
     public final static double HALF_FIELD_LENGTH = FULL_FIELD_LENGTH / 2;
 
-    public final static Pose BLUE_GOAL = new Pose(10, FULL_FIELD_LENGTH - 5);
+    public final static Pose BLUE_GOAL = new Pose(5, FULL_FIELD_LENGTH - 5);
     public final static Pose RED_GOAL = new Pose(FULL_FIELD_LENGTH - BLUE_GOAL.getX(), BLUE_GOAL.getY());
 
     public final static Pose RED_GOAL_GEMS = new Pose (HALF_FIELD_LENGTH + BLUE_GOAL.getX(), BLUE_GOAL.getY());
     public final static Pose BLUE_GOAL_GEMS = new Pose(HALF_FIELD_LENGTH - BLUE_GOAL.getX(), BLUE_GOAL.getY());
 
-    public final static Pose BLUE_GOAL_FAR = new Pose(10, FULL_FIELD_LENGTH);
+    public final static Pose BLUE_GOAL_FAR = new Pose(2, FULL_FIELD_LENGTH);
     public final static Pose RED_GOAL_FAR = new Pose(FULL_FIELD_LENGTH - BLUE_GOAL_FAR.getX(), BLUE_GOAL_FAR.getY());
 }
